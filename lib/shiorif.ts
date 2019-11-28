2da3a5f158ba3262052372f8ea50f7f9d474d8a0
Narazaka <info@narazaka.net>
2019-11-28 16:54:16 +0900

    update deps / eslint

--- lib/shiorif.ts
+++ lib/shiorif.ts
@@ -5,270 +5,280 @@ import * as ShioriJK from "shiorijk";
 import { Shiori } from "shioriloader";
 
 /** The convenient SHIORI Shared Library Interface */
+// eslint-disable-next-line import/export
 export class Shiorif extends EventEmitter {
-  /**
-   * convert array values to Reference* hash
-   * @param headersArray header array values
-   * @return headers hash
-   */
-  static referencesFromArray(headersArray: string[]) {
-    const headers: {[name: string]: string} = {};
-    headersArray.forEach((header, index) => {
-      if (header != null) headers[`Reference${index}`] = header; // tslint:disable-line no-null-keyword
-    });
+    /**
+     * convert array values to Reference* hash
+     * @param headersArray header array values
+     * @return headers hash
+     */
+    static referencesFromArray(headersArray: string[]) {
+        const headers: { [name: string]: string } = {};
+        headersArray.forEach((header, index) => {
+            if (header != null) headers[`Reference${index}`] = header;
+        });
 
-    return headers;
-  }
-
-  private readonly _shiori: Shiori;
-  private readonly _requestParser: ShioriJK.Shiori.Request.Parser;
-  private readonly _responseParser: ShioriJK.Shiori.Response.Parser;
-  private _autoConvertRequestVersion: ShioriConverter.ShioriVersion;
-  private _autoAdjustToResponseCharset: boolean;
-  private _defaultHeaders: {[name: string]: string};
-  private _synchronized: boolean;
-  private _lastResponseCharset?: string;
-  private lock: Promise<void | number | ShioriTransaction> = Promise.resolve();
-
-  /**
-   * @param shiori The instance of SHIORI Shared Library Interface
-   * @param options options
-   */
-  constructor(
-    shiori: Shiori,
-    options: {
-      /** requests will be converted to this version */
-      autoConvertRequestVersion?: ShioriConverter.ShioriVersion;
-      /** request charset header will be set to previous response charset */
-      autoAdjustToResponseCharset?: boolean;
-      /** default headers */
-      defaultHeaders?: {[name: string]: string};
-      /** synchronized mode */
-      synchronized?: boolean;
-    } = {},
-  ) {
-    super();
-    this._shiori = shiori;
-    this.autoConvertRequestVersion = options.autoConvertRequestVersion || "2.6";
-    this.autoAdjustToResponseCharset = options.autoAdjustToResponseCharset || false;
-    this._defaultHeaders = options.defaultHeaders || {};
-    this.synchronized = options.synchronized || false;
-    this._requestParser = new ShioriJK.Shiori.Request.Parser();
-    this._responseParser = new ShioriJK.Shiori.Response.Parser();
-  }
-
-  /** shiori */
-  get shiori() {
-    return this._shiori;
-  }
-
-  /** requests will be converted to this version */
-  get autoConvertRequestVersion() {
-    return this._autoConvertRequestVersion;
-  }
-
-  /** requests will be converted to this version */
-  set autoConvertRequestVersion(version) {
-    this._autoConvertRequestVersion = version;
-  }
-
-  /** request charset header will be set to previous response charset */
-  get autoAdjustToResponseCharset() {
-    return this._autoAdjustToResponseCharset;
-  }
-
-  /** request charset header will be set to previous response charset */
-  set autoAdjustToResponseCharset(enabled) {
-    this._autoAdjustToResponseCharset = enabled;
-  }
-
-  /** default headers */
-  get defaultHeaders() {
-    return this._defaultHeaders;
-  }
-
-  /** default headers */
-  set defaultHeaders(headers) {
-    this._defaultHeaders = headers;
-  }
-
-  /** synchronized mode */
-  get synchronized() {
-    return this._synchronized;
-  }
-
-  /** synchronized mode */
-  set synchronized(synchronized) {
-    this._synchronized = synchronized;
-  }
-
-  /**
-   * SHIORI/2.x/3.x load()
-   *
-   * this emits load(dirpath), loaded(status) events.
-   * @param dirpath The directory that SHIORI Shared Library is placed.
-   *                The end character of dirpath must be the path separator (/ or \\).
-   * @return The status code
-   */
-  load(dirpath: string) {
-    return this.synchronized ? (this.lock = this.lock.then(() => this._load(dirpath))) : this._load(dirpath);
-  }
-
-  private _load(dirpath: string) {
-    this.emit("load", dirpath);
-
-    return this.shiori.load(dirpath).then((status) => {
-      this.emit("loaded", status);
-      if (!status) throw new Shiorif.StatusError();
-
-      return status;
-    });
-  }
-
-  /**
-   * SHIORI/2.x/3.x request()
-   *
-   * this emits request(request), response(response) events.
-   * @param request The SHIORI Request
-   * @param convert enable auto request version convert
-   * @return The SHIORI request transaction
-   */
-  request(request: string | ShioriJK.Message.Request, convert = true) {
-    return this.synchronized ?
-      (this.lock = this.lock.then(() => this._request(request, convert))) :
-      this._request(request, convert);
-  }
-
-  private _request(request: string | ShioriJK.Message.Request, convert = true) {
-    const transaction = new ShioriTransaction();
-    transaction.setRequest(
-      request instanceof ShioriJK.Message.Request ? request : this._requestParser.parse(request),
-    );
-    this.emit("request", transaction);
-    const useRequest = convert
-      ? transaction.request.to(this.autoConvertRequestVersion)
-      : transaction.request;
-    for (const name in this.defaultHeaders) {
-      if (useRequest.headers.header[name] == null) { // tslint:disable-line no-null-keyword
-        useRequest.headers.header[name] = this.defaultHeaders[name];
-      }
-    }
-    if (this.autoAdjustToResponseCharset && this._lastResponseCharset) {
-      useRequest.headers.header["Charset"] = this._lastResponseCharset;
+        return headers;
     }
 
-    return this.shiori.request(useRequest.toString())
-      .then((response) => {
-        transaction.setResponse(this._responseParser.parse(response));
-        this._lastResponseCharset = transaction.response.headers.header["Charset"];
-        this.emit("response", transaction);
+    private readonly _shiori: Shiori;
 
-        return transaction;
-      });
-  }
+    private readonly _requestParser: ShioriJK.Shiori.Request.Parser;
 
-  /**
-   * SHIORI/2.x/3.x request() by SHIORI/3.x request value
-   * @param method method
-   * @param id id
-   * @param headers headers
-   * @param convert enable auto request version convert
-   * @return The SHIORI request transaction
-   */
-  request3(method: ShioriJK.Method, id: string, headers?: Shiorif.Headers, convert = true) {
-    const request = new ShioriJK.Message.Request({
-      request_line: {
-        version: "3.0",
-        method,
-      },
-      headers: {ID: id, ...(headers instanceof Array ? Shiorif.referencesFromArray(headers) : headers)},
-    });
+    private readonly _responseParser: ShioriJK.Shiori.Response.Parser;
 
-    return this.request(request, convert);
-  }
+    private _autoConvertRequestVersion: ShioriConverter.ShioriVersion;
 
-  /**
-   * SHIORI/2.x/3.x request() by SHIORI/2.x request value
-   * @param method method
-   * @param headers headers
-   * @param convert enable auto request version convert
-   * @return The SHIORI request transaction
-   */
-  request2(method: ShioriJK.Method, headers?: Shiorif.Headers, convert = true) {
-    const request = new ShioriJK.Message.Request({
-      request_line: {
-        version: "2.6",
-        method,
-      },
-      headers: headers instanceof Array ? Shiorif.referencesFromArray(headers) : headers,
-    });
+    private _autoAdjustToResponseCharset: boolean;
 
-    return this.request(request, convert);
-  }
+    private _defaultHeaders: { [name: string]: string };
 
-  /**
-   * SHIORI/2.x/3.x request() by GET SHIORI/3.x request value
-   * @param id id
-   * @param headers headers
-   * @param convert enable auto request version convert
-   * @return The SHIORI request transaction
-   */
-  get3(id: string, headers?: Shiorif.Headers, convert = true) {
-    return this.request3("GET", id, headers, convert);
-  }
+    private _synchronized: boolean;
 
-  /**
-   * SHIORI/2.x/3.x request() by NOTIFY SHIORI/3.x request value
-   * @param id id
-   * @param headers headers
-   * @param convert enable auto request version convert
-   * @return The SHIORI request transaction
-   */
-  notify3(id: string, headers?: Shiorif.Headers, convert = true) {
-    return this.request3("NOTIFY", id, headers, convert);
-  }
+    private _lastResponseCharset?: string;
 
-  /**
-   * SHIORI/2.x request() by GET Version SHIORI/2.x request value
-   * @param headers headers
-   * @param convert enable auto request version convert
-   * @return The SHIORI request transaction
-   */
-  getVersion2(headers?: Shiorif.Headers, convert = true) {
-    return this.request2("GET Version", headers, convert);
-  }
+    private lock: Promise<void | number | ShioriTransaction> = Promise.resolve();
 
-  /**
-   * SHIORI/2.x/3.x unload()
-   *
-   * this emits unload(), unloaded(status) events.
-   * @return The status code
-   */
-  unload() {
-    return this.synchronized ? (this.lock = this.lock.then(() => this._unload())) : this._unload();
-  }
+    /**
+     * @param shiori The instance of SHIORI Shared Library Interface
+     * @param options options
+     */
+    constructor(
+        shiori: Shiori,
+        options: {
+            /** requests will be converted to this version */
+            autoConvertRequestVersion?: ShioriConverter.ShioriVersion;
+            /** request charset header will be set to previous response charset */
+            autoAdjustToResponseCharset?: boolean;
+            /** default headers */
+            defaultHeaders?: { [name: string]: string };
+            /** synchronized mode */
+            synchronized?: boolean;
+        } = {},
+    ) {
+        super();
+        this._shiori = shiori;
+        this.autoConvertRequestVersion = options.autoConvertRequestVersion || "2.6";
+        this.autoAdjustToResponseCharset = options.autoAdjustToResponseCharset || false;
+        this._defaultHeaders = options.defaultHeaders || {};
+        this.synchronized = options.synchronized || false;
+        this._requestParser = new ShioriJK.Shiori.Request.Parser();
+        this._responseParser = new ShioriJK.Shiori.Response.Parser();
+    }
 
-  private _unload() {
-    this.emit("unload");
+    /** shiori */
+    get shiori() {
+        return this._shiori;
+    }
 
-    return this.shiori.unload().then((status) => {
-      this.emit("unloaded", status);
-      if (!status) throw new Shiorif.StatusError();
+    /** requests will be converted to this version */
+    get autoConvertRequestVersion() {
+        return this._autoConvertRequestVersion;
+    }
 
-      return status;
-    });
-  }
+    /** requests will be converted to this version */
+    set autoConvertRequestVersion(version) {
+        this._autoConvertRequestVersion = version;
+    }
+
+    /** request charset header will be set to previous response charset */
+    get autoAdjustToResponseCharset() {
+        return this._autoAdjustToResponseCharset;
+    }
+
+    /** request charset header will be set to previous response charset */
+    set autoAdjustToResponseCharset(enabled) {
+        this._autoAdjustToResponseCharset = enabled;
+    }
+
+    /** default headers */
+    get defaultHeaders() {
+        return this._defaultHeaders;
+    }
+
+    /** default headers */
+    set defaultHeaders(headers) {
+        this._defaultHeaders = headers;
+    }
+
+    /** synchronized mode */
+    get synchronized() {
+        return this._synchronized;
+    }
+
+    /** synchronized mode */
+    set synchronized(synchronized) {
+        this._synchronized = synchronized;
+    }
+
+    /**
+     * SHIORI/2.x/3.x load()
+     *
+     * this emits load(dirpath), loaded(status) events.
+     * @param dirpath The directory that SHIORI Shared Library is placed.
+     *                The end character of dirpath must be the path separator (/ or \\).
+     * @return The status code
+     */
+    load(dirpath: string) {
+        // eslint-disable-next-line no-return-assign
+        return this.synchronized ? (this.lock = this.lock.then(() => this._load(dirpath))) : this._load(dirpath);
+    }
+
+    private _load(dirpath: string) {
+        this.emit("load", dirpath);
+
+        return this.shiori.load(dirpath).then(status => {
+            this.emit("loaded", status);
+            if (!status) throw new Shiorif.StatusError();
+
+            return status;
+        });
+    }
+
+    /**
+     * SHIORI/2.x/3.x request()
+     *
+     * this emits request(request), response(response) events.
+     * @param request The SHIORI Request
+     * @param convert enable auto request version convert
+     * @return The SHIORI request transaction
+     */
+    request(request: string | ShioriJK.Message.Request, convert = true) {
+        // eslint-disable-next-line no-return-assign
+        return this.synchronized
+            ? (this.lock = this.lock.then(() => this._request(request, convert)))
+            : this._request(request, convert);
+    }
+
+    private _request(request: string | ShioriJK.Message.Request, convert = true) {
+        const transaction = new ShioriTransaction();
+        transaction.setRequest(
+            request instanceof ShioriJK.Message.Request ? request : this._requestParser.parse(request),
+        );
+        this.emit("request", transaction);
+        const useRequest = convert ? transaction.request.to(this.autoConvertRequestVersion) : transaction.request;
+        // eslint-disable-next-line no-restricted-syntax
+        for (const name in this.defaultHeaders) {
+            if (useRequest.headers.header[name] == null) {
+                useRequest.headers.header[name] = this.defaultHeaders[name];
+            }
+        }
+        if (this.autoAdjustToResponseCharset && this._lastResponseCharset) {
+            useRequest.headers.header.Charset = this._lastResponseCharset;
+        }
+
+        return this.shiori.request(useRequest.toString()).then(response => {
+            transaction.setResponse(this._responseParser.parse(response));
+            this._lastResponseCharset = transaction.response.headers.header.Charset;
+            this.emit("response", transaction);
+
+            return transaction;
+        });
+    }
+
+    /**
+     * SHIORI/2.x/3.x request() by SHIORI/3.x request value
+     * @param method method
+     * @param id id
+     * @param headers headers
+     * @param convert enable auto request version convert
+     * @return The SHIORI request transaction
+     */
+    request3(method: ShioriJK.Method, id: string, headers?: Shiorif.Headers, convert = true) {
+        const request = new ShioriJK.Message.Request({
+            request_line: {
+                version: "3.0",
+                method,
+            },
+            headers: { ID: id, ...(headers instanceof Array ? Shiorif.referencesFromArray(headers) : headers) },
+        });
+
+        return this.request(request, convert);
+    }
+
+    /**
+     * SHIORI/2.x/3.x request() by SHIORI/2.x request value
+     * @param method method
+     * @param headers headers
+     * @param convert enable auto request version convert
+     * @return The SHIORI request transaction
+     */
+    request2(method: ShioriJK.Method, headers?: Shiorif.Headers, convert = true) {
+        const request = new ShioriJK.Message.Request({
+            request_line: {
+                version: "2.6",
+                method,
+            },
+            headers: headers instanceof Array ? Shiorif.referencesFromArray(headers) : headers,
+        });
+
+        return this.request(request, convert);
+    }
+
+    /**
+     * SHIORI/2.x/3.x request() by GET SHIORI/3.x request value
+     * @param id id
+     * @param headers headers
+     * @param convert enable auto request version convert
+     * @return The SHIORI request transaction
+     */
+    get3(id: string, headers?: Shiorif.Headers, convert = true) {
+        return this.request3("GET", id, headers, convert);
+    }
+
+    /**
+     * SHIORI/2.x/3.x request() by NOTIFY SHIORI/3.x request value
+     * @param id id
+     * @param headers headers
+     * @param convert enable auto request version convert
+     * @return The SHIORI request transaction
+     */
+    notify3(id: string, headers?: Shiorif.Headers, convert = true) {
+        return this.request3("NOTIFY", id, headers, convert);
+    }
+
+    /**
+     * SHIORI/2.x request() by GET Version SHIORI/2.x request value
+     * @param headers headers
+     * @param convert enable auto request version convert
+     * @return The SHIORI request transaction
+     */
+    getVersion2(headers?: Shiorif.Headers, convert = true) {
+        return this.request2("GET Version", headers, convert);
+    }
+
+    /**
+     * SHIORI/2.x/3.x unload()
+     *
+     * this emits unload(), unloaded(status) events.
+     * @return The status code
+     */
+    unload() {
+        // eslint-disable-next-line no-return-assign
+        return this.synchronized ? (this.lock = this.lock.then(() => this._unload())) : this._unload();
+    }
+
+    private _unload() {
+        this.emit("unload");
+
+        return this.shiori.unload().then(status => {
+            this.emit("unloaded", status);
+            if (!status) throw new Shiorif.StatusError();
+
+            return status;
+        });
+    }
 }
 
+// eslint-disable-next-line no-redeclare, import/export
 export namespace Shiorif {
-  /** load() / unload() status error */
-  export class StatusError extends Error {
-  }
+    /** load() / unload() status error */
+    export class StatusError extends Error {}
 
-  /**
-   * headers
-   *
-   * array = Reference* / hash = general
-   */
-  export type Headers = {[name: string]: string} | string[];
+    /**
+     * headers
+     *
+     * array = Reference* / hash = general
+     */
+    export type Headers = { [name: string]: string } | string[];
 }
--- test/basic.ts
+++ test/basic.ts
@@ -1,27 +1,27 @@
 /// <reference types="mocha" />
-// tslint:disable no-implicit-dependencies
+/* eslint-disable class-methods-use-this, @typescript-eslint/no-unused-vars */
 import * as assert from "power-assert";
 import { Shiori } from "shioriloader";
 import { Shiorif } from "../lib/shiorif";
 
 class TestShiori implements Shiori {
-  load(_: string) { // tslint:disable-line prefer-function-over-method
-    return Promise.resolve(1);
-  }
+    load(_: string) {
+        return Promise.resolve(1);
+    }
 
-  unload() { // tslint:disable-line prefer-function-over-method
-    return Promise.resolve(1);
-  }
+    unload() {
+        return Promise.resolve(1);
+    }
 
-  request(_: string) { // tslint:disable-line prefer-function-over-method
-    return Promise.resolve("");
-  }
+    request(_: string) {
+        return Promise.resolve("");
+    }
 }
 
 describe("ShioriTransaction", () => {
-  it("can initialize", () => {
-    const shiori = new TestShiori();
-    const shiorif = new Shiorif(shiori);
-    assert(shiorif);
-  });
+    it("can initialize", () => {
+        const shiori = new TestShiori();
+        const shiorif = new Shiorif(shiori);
+        assert(shiorif);
+    });
 });
