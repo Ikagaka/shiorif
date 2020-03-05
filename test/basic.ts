cd4f57691312c8fb3234bcd2a617b6da86cb37fe
Narazaka <info@narazaka.net>
2020-03-05 17:42:54 +0900

    more test

--- test/basic.ts
+++ test/basic.ts
@@ -5,7 +5,12 @@ import { Shiori } from "shioriloader";
 import { Shiorif } from "../lib/shiorif";
 
 class TestShiori implements Shiori {
-    load(_: string) {
+    dirpath?: string;
+
+    lastRequest?: string;
+
+    load(dirpath: string) {
+        this.dirpath = dirpath;
         return Promise.resolve(1);
     }
 
@@ -13,8 +18,9 @@ class TestShiori implements Shiori {
         return Promise.resolve(1);
     }
 
-    request(_: string) {
-        return Promise.resolve("");
+    request(req: string) {
+        this.lastRequest = req;
+        return Promise.resolve("SHIORI/3.0 200 OK\x0d\x0a\x0d\x0a");
     }
 }
 
@@ -23,5 +29,26 @@ describe("ShioriTransaction", () => {
         const shiori = new TestShiori();
         const shiorif = new Shiorif(shiori);
         assert(shiorif);
+    });
+
+    it("can load", async () => {
+        const shiori = new TestShiori();
+        const shiorif = new Shiorif(shiori);
+        assert((await shiorif.load("foo")) === 1);
+        assert(shiori.dirpath === "foo");
+    });
+
+    it("can request", async () => {
+        const shiori = new TestShiori();
+        const shiorif = new Shiorif(shiori);
+        const res = (await shiorif.request3("GET", "OnFoo")).response;
+        assert(res.status_line.code === 200);
+        assert(shiori.lastRequest === "GET Sentence SHIORI/2.6\x0d\x0aEvent: OnFoo\x0d\x0a\x0d\x0a");
+    });
+
+    it("can unload", async () => {
+        const shiori = new TestShiori();
+        const shiorif = new Shiorif(shiori);
+        assert((await shiorif.unload()) === 1);
     });
 });
